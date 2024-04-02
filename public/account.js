d19531c8a3f7491fe3e6615c685eeb568d6beef3
Codycas7 <80531354+Codycas7@users.noreply.github.com>
2024-04-02 15:22:01 +0000

    auth 1

--- public/account.js
+++ public/account.js
@@ -1,33 +1,42 @@
-const acctNum = 0;
-class Acct {
-    constructor (uName, email, pWord) {
-        this.uName = uName;
-        this.email = email;
-        this.pWord = pWord;
-    }
-}
-let accts = [];
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+    getAuth,
+    connectAuthEmulator,
+    signInWithEmailAndPassword
+} from "firebase/auth";
 
-function createAccount () {
-    const uName = document.getElementById("uName");
-    const email = document.getElementById("createEmail");
-    const pWord = document.getElementById("createPassword");
+import {
+    uName,
+    createEmail,
+    createPassword,
+    signUpButton
+} from "./createAcct.html";
 
-    if (uName.value.length == 0 || email.value.length == 0 || pWord.value.length == 0) {
-        document.getElementById("createAcctBlankError").style.visibility="visible";
-        document.getElementById("createAcctPasswordError").style.visibility="hidden";
-    }
-    else if (pWord.value.length < 8) {
-        document.getElementById("createAcctPasswordError").style.visibility="visible";
-        document.getElementById("createAcctBlankError").style.visibility="hidden";
-    }
-    else {
-        document.getElementById("createAcctPasswordError").style.visibility="hidden";
-        accts.push(new Acct (uName.value, email.value, pWord.value));
-        console.log(accts);
-    }
+const firebaseConfig = {
+    apiKey: "AIzaSyBQiD0QmWyl_-4SHfUoFFsILPxJ8F5tHKA",
+    authDomain: "cody-cooks.firebaseapp.com",
+    databaseURL: "https://cody-cooks-default-rtdb.firebaseio.com",
+    projectId: "cody-cooks",
+    storageBucket: "cody-cooks.appspot.com",
+    messagingSenderId: "860432436020",
+    appId: "1:860432436020:web:d76240c51638608eea7361",
+    measurementId: "G-XL8XSHVV7Z"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const analytics = getAnalytics(app);
+
+connectAuthEmulator(auth, "http://localhost:9099");
+
+//signup
+const loginEmailPassword = async () => {
+    const loginEmail = createEmail.value;
+    const loginPassword = createPassword.value;
 }
 
-function switchPage () {
-    
-}
+signUpButton.addEventListener("click", loginEmailPassword);
+console.log(userCredential.user);
+
+const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
